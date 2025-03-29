0159f956e11ca52ed7c4c74e89aec2646d44c877
ARST113 <96774211+ARST113@users.noreply.github.com>
2025-03-29 23:33:54 +0300

    Update plugin-old-interface-hover.js

--- plugin-old-interface-hover.js
+++ plugin-old-interface-hover.js
@@ -3,68 +3,60 @@ Lampa.Platform.tv();
 (function () {
     'use strict';
 
-    console.log('[SorterPlugin] загружен');
+    console.log('[SorterPlugin] плагин загружен');
 
     function startPlugin() {
         try {
+            // Сброс приоритета, если задан
             if (Lampa.Storage.get('full_btn_priority') !== undefined) {
                 Lampa.Storage.set('full_btn_priority', '{}');
             }
 
-            Lampa.Listener.follow('full', function (e) {
+            Lampa.Listener.follow('full', function(e) {
                 if (e.type === 'complite') {
-                    setTimeout(function () {
+                    setTimeout(function() {
                         try {
-                            const fullContainer = e.object.activity.render();
-                            const targetContainer = fullContainer.find('.full-start-new__buttons');
-                            const allButtons = fullContainer.find('.buttons--container .full-start__button')
+                            var fullContainer = e.object.activity.render();
+                            var targetContainer = fullContainer.find('.full-start-new__buttons');
+                            console.log('[SorterPlugin] Контейнер найден:', targetContainer);
+
+                            // Собираем все кнопки из двух контейнеров
+                            var allButtons = fullContainer.find('.buttons--container .full-start__button')
                                 .add(targetContainer.find('.full-start__button'));
 
-                            const cinema = allButtons.filter('.cinema');
-                            const torrent = allButtons.filter('.torrent');
-                            const online = allButtons.filter(function () {
-                                return $(this).attr('class').includes('online');
-                            });
-                            const trailers = allButtons.filter(function () {
-                                return $(this).attr('class').includes('trailer');
-                            });
-                            const other = allButtons.not('.cinema, .torrent')
-                                .filter(function () {
-                                    const cls = $(this).attr('class');
-                                    return !cls.includes('online') && !cls.includes('trailer');
-                                });
+                            // Определяем группы по классам
+                            var cinemaButtons  = allButtons.filter('.cinema');
+                            var torrentButtons = allButtons.filter('.torrent');
+                            var onlineButtons  = allButtons.filter('.online');
+                            var trailerButtons = allButtons.filter('.trailer');
+                            var otherButtons   = allButtons.not('.cinema, .torrent, .online, .trailer');
 
-                            const orderedButtons = []
-                                .concat(cinema.get())
-                                .concat(torrent.get())
-                                .concat(online.get())
-                                .concat(trailers.get())
-                                .concat(other.get());
+                            console.log('[SorterPlugin] Количество: Cinema:', cinemaButtons.length, 
+                                'Torrent:', torrentButtons.length, 
+                                'Online:', onlineButtons.length, 
+                                'Trailer:', trailerButtons.length, 
+                                'Остальные:', otherButtons.length);
 
-                            // Удаляем старые, кроме button--play (если нужен)
-                            targetContainer.find('.full-start__button').not('.button--play').remove();
+                            // Отсоединяем кнопки, чтобы сохранить события
+                            cinemaButtons.detach();
+                            torrentButtons.detach();
+                            onlineButtons.detach();
+                            trailerButtons.detach();
+                            otherButtons.detach();
 
-                            // Добавляем в порядке
-                            orderedButtons.forEach(btn => {
+                            // Формируем новый порядок
+                            var newOrder = cinemaButtons.get()
+                                .concat(torrentButtons.get(), onlineButtons.get(), trailerButtons.get(), otherButtons.get());
+
+                            // Очищаем контейнер (это безопасно, т.к. кнопки уже отсоединены)
+                            targetContainer.empty();
+
+                            // Вставляем кнопки в новом порядке
+                            newOrder.forEach(function(btn) {
                                 targetContainer.append(btn);
                             });
 
                             Lampa.Controller.toggle("full_start");
-                            console.log('[SorterPlugin] порядок обновлён');
+                            console.log('[SorterPlugin] Новый порядок применён');
                         } catch (err) {
-                            console.error('[SorterPlugin] ошибка сортировки:', err);
-                        }
-                    }, 100);
-                }
-            });
-
-            if (typeof module !== 'undefined' && module.exports) {
-                module.exports = {};
-            }
-        } catch (err) {
-            console.error('[SorterPlugin] ошибка запуска:', err);
-        }
-    }
-
-    startPlugin();
-})();
+                            console.error('[SorterPlugin] Ошибка сортировки кноп
